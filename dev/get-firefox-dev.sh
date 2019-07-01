46c8bb2347d935745b0c3e4792e2a500c45e2457
icebowl <icebowl1967@gmail.com>
2019-07-01 05:03:39 -0700

    AA

--- dev/get-firefox-dev.sh
+++ dev/get-firefox-dev.sh
@@ -1,3 +1,4 @@
+!/bin/bash
 rm '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US' ;
 curl -LO "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US";
 tar -xvf '?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US';
